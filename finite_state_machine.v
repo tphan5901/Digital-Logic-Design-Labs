f40f5dea3ab8276708199f0ef00b587a46d5cfef
Your Name <tphan5901@gmail.com>
2026-07-08 15:53:42 -0700

    state machine practice

--- finite_state_machine.v
+++ finite_state_machine.v
@@ -31,7 +31,7 @@ module finite_state_machine(
             E: if(x) next_state = D;
                 else next_state = B;
             default: next_state = A;
-            
+
         endcase
 
         assign y = (current_state == D | current_state == E);
@@ -39,3 +39,67 @@ module finite_state_machine(
     end
 
 endmodule
+
+
+//practice
+module pair_detect(
+    input clk,
+    input inbits,
+    input reset,
+    output reg detect
+);
+
+    reg[1:0] state;
+
+    initial begin
+        //starting state of 00
+        state = 2'b00;
+    end
+
+    always@(posedge clk, posedge reset)
+    begin
+        if(reset)
+            state <= 2'b00;
+        else
+            begin
+                case(state)
+                2'b00:
+                begin
+                    if(inbits) state <= 2'b01;
+                    else state <= 2'b10;
+
+                end
+
+                2'b01:
+                begin
+                    if (inbits) state <= 2'b11;
+                    else state <= 2'b10;
+                end
+
+                2'b10:
+                begin
+                    if (inbits) state <= 2'b01;
+                    else state <= 2'b10;
+                end
+
+                2'b11:
+                begin
+                    if (inbits) state <= 2'b01;
+                    else state <= 2'b10;
+                end
+
+                endcase
+                
+            end
+    end
+
+    always @(posedge clk, posedge reset) begin
+        if (reset) 
+            detect <= 0;
+        else if (state == 2'b11)
+            detect <= 1;
+        else detect <= 0;
+    end
+
+endmodule
+
