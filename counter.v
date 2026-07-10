1eb30619b0b70ca5f4712f499471b793e091a33a
Your Name <tphan5901@gmail.com>
2026-07-09 23:47:18 -0700

    added counter

--- counter.v
+++ counter.v
@@ -1,6 +1,6 @@
 
 //counter as timers
-module timer_parameter #(parameter FINAL_VALUE = 255 ) (
+module timer_parameter #(parameter FINAL_VALUE = 255) (
     input clk,
     input reset_n,
     input enable,
@@ -8,23 +8,53 @@ module timer_parameter #(parameter FINAL_VALUE = 255 ) (
     output done
 );
 
+    //register size
     localparam BITS = $clog2(FINAL_VALUE);
+    //two registers, (reg stands for register)
     reg [BITS - 1:0] Q_reg, Q_next;
 
     always @(posedge clk, negedge reset_n) begin
+    // reset signal
         if (~reset_n)
+    // b0 means 00000000 so count is 0
             Q_reg <= 'b0;
+    // if enable signal, then start timer
         else if(enable)
+    // current count(Q_reg) = new count(Q_next) 
             Q_reg <= Q_next;
         else
             Q_reg <= Q_reg;
     end
 
-    //next state
+    //assign checks if q_reg = final_value, if so then done = 1
     assign done = Q_reg == FINAL_VALUE;
 
     always @(*) begin
-        Q_next = done? 'b0: Q_reg + 1;
+    //    Q_next = done? 'b0: Q_reg + 1;   
+
+    //if else statement is equilvalent of ternary operator above
+        if (done)
+            Q_next = 0;
+        //keep incrementing the count
+        else
+            Q_next = Q_reg + 1;
     end
+
+endmodule
+
+//testbench for timer_parameter method
+module timer_parameter_tb();
+    localparam FINAL_VALUE = 49_999;
+    localparam bits = $clog2(FINAL_VALUE);
+
+    reg clk, reset_n, enable;
+    wire done;
+
+    //call method, timer_parameter
+    timer_parameter #(.FINAL_VALUE(FINAL_VALUE)) uut (
+        .clk(clk),
+        .reset_n(reset_n),
+        .enable(enable)
+    );
 
 endmodule
